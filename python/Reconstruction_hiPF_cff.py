5e941313d2d032422189918a7ae6dfb64a943592
cmsbuild <>
2012-03-05 09:44:55 +0000

    This commit was manufactured by cvs2git to create tag 'RecoHI-Configuration- CMSSW_5_2_4_hltpatch2'.
    Sprout from master 2011-10-18 21:26:59 UTC Matthew Nguyen <sha1-7db6f116e60aa37215336807cfe2bf96dbb4b60b@cern.ch> 'Revert back to sequence without electrons, electrons are now optional in Configuration.StandardSequences.ReconstructionHeavyIons_cff.py'
    Cherrypick from master 2012-03-05 09:44:45 UTC Matthew Nguyen <sha1-7db6f116e60aa37215336807cfe2bf96dbb4b60b@cern.ch> 'Change to MVA electron selection as in RecoParticleFlow_cff':
        python/Reconstruction_hiPF_cff.py

--- python/Reconstruction_hiPF_cff.py
+++ python/Reconstruction_hiPF_cff.py
@@ -2,6 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 # include  particle flow local reconstruction
 from RecoParticleFlow.PFClusterProducer.particleFlowCluster_cff import *
+particleFlowClusterPS.thresh_Pt_Seed_Endcap = cms.double(99999.)
 
 from RecoParticleFlow.PFTracking.pfTrack_cfi import *
 pfTrack.UseQuality = cms.bool(True)
@@ -16,7 +17,9 @@ particleFlowBlock.useConvBremPFRecTracks = cms.bool(False)
 particleFlowBlock.useIterTracking = cms.bool(False)
 particleFlowBlock.useNuclear = cms.bool(False)
 particleFlowBlock.useConversions = cms.bool(False)
+particleFlowBlock.RecMuons = 'muons'
 
+particleFlowTmp.postMuonCleaning = cms.bool(False)
 particleFlowTmp.vertexCollection = cms.InputTag("hiSelectedVertex")
 particleFlowTmp.usePFElectrons = cms.bool(True)
 particleFlowTmp.muons = cms.InputTag("muons")
@@ -26,7 +29,7 @@ from RecoParticleFlow.PFTracking.pfTrackElec_cfi import *
 pfTrackElec.applyGsfTrackCleaning = cms.bool(True)
 pfTrackElec.PrimaryVertexLabel = cms.InputTag("hiSelectedVertex")
 
-electronsCiCLoose.verticesCollection = cms.InputTag("hiSelectedVertex")
+mvaElectrons.vertexTag = cms.InputTag("hiSelectedVertex")
 
 # local reco must run before electrons (RecoHI/HiEgammaAlgos), due to PF integration
 HiParticleFlowLocalReco = cms.Sequence(particleFlowCluster
@@ -35,7 +38,7 @@ HiParticleFlowLocalReco = cms.Sequence(particleFlowCluster
                                        )
 
 #PF Reco runs after electrons
-HiParticleFlowReco = cms.Sequence(pfGsfElectronCiCSelectionSequence
+HiParticleFlowReco = cms.Sequence(pfGsfElectronMVASelectionSequence
                                   * particleFlowBlock
                                   * particleFlowTmp
                                   )
